beb272979ba54bd6dbdf5ef1cf8adb3f3cfa6410
WJerry11 <136723953+WJerry11@users.noreply.github.com>
2023-07-15 15:40:48 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let genrateDomain = () => {
+  let first = ["cat", "apple", "television"];
+  let second = ["pony", "dog", "rooster"];
+  let third = [".com", ".org", ".gov", ".net"];
+  for (let i = 0; i < first.length; i++)
+    for (let a = 0; a < second.length; a++)
+      for (let b = 0; b < third.length; b++)
+        console.log(first[i] + second[a] + third[b]);
 };
+console.log(genrateDomain());
