9dddfeea9ddc1729f26351c665ea423cb671023b
WJerry11 <136723953+WJerry11@users.noreply.github.com>
2023-07-18 16:43:20 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 let genrateDomain = () => {
+  let domainName = [];
   let first = ["cat", "apple", "television"];
   let second = ["pony", "dog", "rooster"];
   let third = [".com", ".org", ".gov", ".net"];
-  for (let i = 0; i < first.length; i++)
-    for (let a = 0; a < second.length; a++)
-      for (let b = 0; b < third.length; b++)
-        console.log(first[i] + second[a] + third[b]);
+  for (let i = 0; i < first.length; i++) {
+    for (let a = 0; a < second.length; a++) {
+      for (let b = 0; b < third.length; b++) {
+        let result = domainName.push(first[i] + second[a] + third[b]);
+      }
+    }
+  }
+  return domainName;
 };
-console.log(genrateDomain());
+let allDomain = genrateDomain();
+for (let c = 0; c < allDomain.length; c++) {
+  document.getElementById("demo").innerHTML += "<p>" + allDomain[c] + "</p>";
+}
